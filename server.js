a8fe3ebf3cbdfbb62f3acb55feaf7c85e9a506c0
Dylan Kazunaga <dylankazunaga@Dylans-MacBook-Air.local>
2023-06-14 18:10:38 -0700

    updated server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET Route for feedback page
